229469a6a2d9a0e9af9a00422fc75b96ed4ea602
Luka Licheli <lukalicheli@gmail.com>
2023-03-14 06:02:39 -0700

    Finished icons

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,5 +1,11 @@
 import "./App.css";
-import { GrHomeRounded } from "react-icons/gr";
+import { GrHomeRounded, GrCompass } from "react-icons/gr";
+import { GoSearch } from "react-icons/go";
+import { ImCompass2 } from "react-icons/im";
+import { BiMoviePlay } from "react-icons/bi";
+import { RiMessengerLine } from "react-icons/ri";
+import { AiOutlineHeart, AiOutlineInstagram } from "react-icons/ai";
+import { FiPlusSquare } from "react-icons/fi";
 
 function App() {
   return (
@@ -7,18 +13,49 @@ function App() {
       <nav>
         <ul>
           <li>
-            <GrHomeRounded className="icon" fill="white" href="#home" size={30}>
+            <AiOutlineInstagram
+              className="ig-icon"
+              href="#instagram"
+              fill="white"
+              size={30}
+            >
+              Instagram
+            </AiOutlineInstagram>
+          </li>
+          <li>
+            <GrHomeRounded fill="white" href="#home" size={30}>
               Home
             </GrHomeRounded>
           </li>
           <li>
-            <a href="#news">News</a>
+            <GoSearch href="#search" fill="white" size={30}>
+              Search
+            </GoSearch>
           </li>
           <li>
-            <a href="#contact">Contact</a>
+            <ImCompass2 href="#explore" fill="white" size={30}>
+              Explore
+            </ImCompass2>
           </li>
           <li>
-            <a href="#about">About</a>
+            <BiMoviePlay href="#reels" fill="white" size={30}>
+              Reels
+            </BiMoviePlay>
+          </li>
+          <li>
+            <RiMessengerLine href="#messenger" fill="white" size={30}>
+              Messenger
+            </RiMessengerLine>
+          </li>
+          <li>
+            <AiOutlineHeart href="#notifications" fill="white" size={30}>
+              Notifications
+            </AiOutlineHeart>
+          </li>
+          <li>
+            <FiPlusSquare href="#create" color="white" size={30}>
+              Create
+            </FiPlusSquare>
           </li>
         </ul>
       </nav>
