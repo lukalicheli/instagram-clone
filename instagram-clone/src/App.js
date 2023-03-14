b98cea5a1f828d27daa9f3f608eefdbcc2962d8c
Luka Licheli <lukalicheli@gmail.com>
2023-03-14 04:35:02 -0700

    Created navbar and the necessary components for the future dev

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <nav>
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#news">News</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
